265845fd08055303e93693fe3fe96ae3348d7145
Luti <wns6619@gmail.com>
2025-10-26 20:34:05 +0900

    initial commit

---
+++ src/test/java/luti/server/LillingApplicationTests.java
@@ -0,0 +1,13 @@
+package luti.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
