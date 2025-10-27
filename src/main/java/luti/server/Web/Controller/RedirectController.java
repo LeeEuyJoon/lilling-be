1cb50b1f54ad0732b73f5796cb2268741f1ffcba
Luti <wns6619@gmail.com>
2025-10-27 11:07:08 +0900

    clean up import

--- src/main/java/luti/server/Web/Controller/RedirectController.java
+++ src/main/java/luti/server/Web/Controller/RedirectController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import luti.server.Facade.RedirectFacade;
-import luti.server.Service.UrlService;
 
 @RestController
 @RequestMapping("/")
