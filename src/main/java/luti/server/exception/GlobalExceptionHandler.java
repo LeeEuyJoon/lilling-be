c8acf29bbda50d623a7fcca1bee2565c8f1193db
Luti <wns6619@gmail.com>
2026-09-24 01:01:04 +0900

    add NoResourceFoundException handler

--- src/main/java/luti/server/exception/GlobalExceptionHandler.java
+++ src/main/java/luti/server/exception/GlobalExceptionHandler.java
@@ -2,15 +2,23 @@ package luti.server.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+	@ExceptionHandler(NoResourceFoundException.class)
+	public ResponseEntity<Void> handleNoResourceFoundException(NoResourceFoundException ex, WebRequest request) {
+		log.debug("정적 리소스 요청 (봇 스캔 등 예상된 404): uri={}", request.getDescription(false));
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception ex, WebRequest request) {
